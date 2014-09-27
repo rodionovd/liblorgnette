725d5aebb0677ec416716dbccbe8a97696929b5d
rodionovd <i.am.rodionovd@gmail.com>
2014-09-28 01:42:54 +0700

    Initial commit

---
+++ lorgnette.h
@@ -0,0 +1,38 @@
+//
+//  lorgnette.h
+//  liblorgnette
+//
+//  Created by Dmitry Rodionov on 9/24/14.
+//  Copyright (c) 2014 rodionovd. All rights reserved.
+//
+#include <mach/mach.h>
+#pragma once
+/**
+ * @abstract
+ * Look ap a symbol adress inside an arbitrary process' address space.
+ *
+ * @note
+ * This function iterates local symbols first and only then it looks for symbols
+ * in linked libraries.
+ *
+ * @param target
+ * The target process to inspect.
+ * @param symbol_name
+ * The name of the symbol to find. This parameter must not be NULL.
+ *
+ * @return
+ * An address of the given symbol within the given process, or 0 (zero) if this symbol
+ * could not be found.
+ *
+ * @b Examples
+ *
+ * Find a @p dlopen symbol address within the current task memory space
+ * @code
+ * addr = lorgnette_lookup(mach_task_self(), "dlopen");
+ * @endcode
+ * Find a @p glob_var0 symbol address inside a remote process
+ * @code
+ * addr = lorgnette_lookup(some_task, "glob_var0");
+ * @endcode
+ */
+mach_vm_address_t lorgnette_lookup(task_t target, const char *symbol_name);
---
+++ tests/main.c
@@ -0,0 +1,90 @@
+//
+//  main.c
+//  lorgnette_demo
+//
+//  Created by Dmitry Rodionov on 9/24/14.
+//  Copyright (c) 2014 rodionovd. All rights reserved.
+//
+#include <stdio.h>
+#include <assert.h>
+/* For tests */
+#include <dlfcn.h>
+#include <curl/curl.h>
+
+#include "lorgnette.h"
+
+#pragma mark - Local tests
+
+void test_local_function_lookup(void)
+{
+    // given
+    task_t self = mach_task_self();
+    // when
+    mach_vm_address_t demo_function_addr = lorgnette_lookup(self, "demo_function");
+    mach_vm_address_t main_addr = lorgnette_lookup(self, "main");
+    // then
+    assert((mach_vm_address_t)dlsym(RTLD_DEFAULT, "demo_function") == demo_function_addr);
+    assert((mach_vm_address_t)dlsym(RTLD_DEFAULT, "main") == main_addr);
+}
+
+void test_local_sharedcached_function_lookup(void)
+{
+    // given
+    task_t self = mach_task_self();
+    // when
+    mach_vm_address_t fprintf_addr = lorgnette_lookup(self, "fprintf");
+    mach_vm_address_t dlopen_addr = lorgnette_lookup(self, "dlopen");
+    // then
+    assert((mach_vm_address_t)dlsym(RTLD_DEFAULT, "fprintf") == fprintf_addr);
+    assert((mach_vm_address_t)dlsym(RTLD_DEFAULT, "dlopen") == dlopen_addr);
+}
+
+void test_local_linked_library_function_lookup(void)
+{
+    // given
+    task_t self = mach_task_self();
+    // when
+    mach_vm_address_t curl_easy_init_addr = lorgnette_lookup(self, "curl_easy_init");
+    // then
+    assert((mach_vm_address_t)&curl_easy_init == curl_easy_init_addr);
+}
+
+#pragma mark - Remote tests
+
+void test_remote_function_lookup(void)
+{
+    // given
+    task_t target;
+    int err = KERN_FAILURE;
+    // when
+    err = task_for_pid(mach_task_self(), 1 /* launchd */, &target);
+    mach_vm_address_t remote_dlopen_addr = lorgnette_lookup(target, "dlopen");
+    assert(err == KERN_SUCCESS);
+    // then
+    assert(err == KERN_SUCCESS);
+    assert(remote_dlopen_addr > 0);
+//    fprintf(stderr, "Remote dlopen @ 0x%llx, local @ %p\n", remote_dlopen_addr, dlsym(RTLD_DEFAULT, "dlopen"));
+}
+
+
+#pragma mark - Misc
+
+void __attribute__ ((visibility ("default")))
+demo_function(void)
+{
+    printf("%s", __FUNCTION__);
+}
+
+int __attribute__ ((visibility ("default")))
+main(int argc, const char * argv[])
+{
+    /* Local tests */
+    test_local_function_lookup();
+    test_local_sharedcached_function_lookup();
+    test_local_linked_library_function_lookup();
+
+    /* Remote tests */
+    test_remote_function_lookup();
+
+    return 0;
+}
